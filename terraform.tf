382a2a51daa0c4ed81ef65743a69ef2352e1a1ce
Milan Vojnovič <milanvojnovic@gmail.com>
2026-08-14 19:29:19 +0200

    feat(state): migrate to remote S3 (#24)
    The S3 bucket was created manually (general, versioning and encryption
    enabled).

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.12.0"
+
+  backend "s3" {
+    bucket       = "milanoid-labs-terraform-tofu-state"
+    key          = "prod/terraform.tfstate"
+    use_lockfile = true
+    region       = "eu-central-1"
+    profile      = "milanoid" # this will work on my machine only, if tofu running in CI it won't work
+    encrypt      = true       # to make it explicit, S3 default encryption is enabled in AWS
+  }
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
