3ae010ed5990090ddd5366b18cdf262c5022a4af
milanoid <milanvojnovic@gmail.com>
2026-08-16 13:45:37 +0200

    ci(tofu): apply workflow

--- terraform.tf
+++ terraform.tf
@@ -6,7 +6,7 @@ terraform {
     key          = "prod/terraform.tfstate"
     use_lockfile = true
     region       = "eu-central-1"
-    # profile      = "milanoid" # uncomment this if running locally
+    # profile      = "milanoid" # uncomment this if running locally, or rather run 'export AWS_PROFILE=milanoid'
     encrypt = true # to make it explicit, S3 default encryption is enabled in AWS
   }
 
