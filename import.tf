237b7e44e763d4af46ce9922f367795f819190e1
milanoid <milanvojnovic@gmail.com>
2026-08-24 14:40:57 +0200

    feat: import renovate-minimumReleaseAge rule
    Status check rule to not allow merging Renovate PRs without passing
    (currently 3 days) minimumReleaseAge

--- import.tf
+++ import.tf
@@ -2,3 +2,8 @@ import {
   to = github_repository.this["import-me-tofu"]
   id = "import-me-tofu"
 }
+
+import {
+  to = github_repository_ruleset.homelab_cluster
+  id = "homelab-cluster:21289334"
+}
